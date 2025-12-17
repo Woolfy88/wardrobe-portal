c2355b719c48ca370a807f63b1d6bd398a954fd7
Woolfy88 <jwoolfenden@hdhc.com>
2025-12-17 20:18:39 +0000

    Update 7_Product_Info.py

--- pages/7_Product_Info.py
+++ pages/7_Product_Info.py
@@ -1,64 +1,22 @@
 import streamlit as st
 
 st.set_page_config(page_title="Product Info", layout="wide")
-st.title("Product Information")
-
-st.write(
-    "Use this page to view product details, reference images, and installation notes."
-)
-
-st.divider()
-
-# -----------------------------------
-# Aura Stanchions
-# -----------------------------------
-st.subheader("Aura Stanchions")
-
-st.image(
-    "assets/product_info/aura.jpg",
-    caption="Aura Stanchion System",
-    use_column_width=True,
-)
-
-st.markdown(
-    """
-"""
-)
-
-st.divider()
-
-# -----------------------------------
-# Door Types
-# -----------------------------------
-st.subheader("Door Types")
-
-st.image(
-    "assets/product_info/Door Types.jpg",
-    caption="Our Types Of Doors",
-    use_column_width=True,
-)
-
-st.markdown(
-    """
-
-"""
-)
-
-st.divider()
-
-# -----------------------------------
-#  Woodwork Colours
-# -----------------------------------
-st.subheader("Soft-Close Units")
+st.title("Product Info")
 
 st.image(
     "assets/product_info/Colours.jpg",
-    caption="Our Woodwork Colours",
+    caption="Colours",
     use_column_width=True,
 )
 
-st.markdown(
-    """
+st.image(
+    "assets/product_info/Door Types.jpg",
+    caption="Door Types",
+    use_column_width=True,
+)
 
-"""
+st.image(
+    "assets/product_info/Relax.jpg",
+    caption="Relax",
+    use_column_width=True,
 )

d7d5c11e7d81629e9e25d41537ece373c6fae9fa
Woolfy88 <jwoolfenden@hdhc.com>
2025-12-17 20:24:03 +0000

    Update 7_Product_Info.py

--- pages/7_Product_Info.py
+++ pages/7_Product_Info.py
@@ -4,19 +4,25 @@ st.set_page_config(page_title="Product Info", layout="wide")
 st.title("Product Info")
 
 st.image(
-    "assets/product_info/Colours.jpg",
+    "assets/product_info/Aura.JPG",
+    caption="Aura",
+    use_column_width=True,
+)
+
+st.image(
+    "assets/product_info/Colours.JPG",
     caption="Colours",
     use_column_width=True,
 )
 
 st.image(
-    "assets/product_info/Door Types.jpg",
+    "assets/product_info/Door Types.JPG",
     caption="Door Types",
     use_column_width=True,
 )
 
 st.image(
-    "assets/product_info/Relax.jpg",
+    "assets/product_info/Relax.JPG",
     caption="Relax",
     use_column_width=True,
 )

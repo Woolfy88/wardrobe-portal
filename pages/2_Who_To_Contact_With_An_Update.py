ae58f21848b41c847e0be4d4e44d1cf3f76c7547
Woolfy88 <jwoolfenden@hdhc.com>
2025-12-17 20:40:33 +0000

    Update 2_Who_To_Contact_With_An_Update.py

--- pages/2_Who_To_Contact_With_An_Update.py
+++ pages/2_Who_To_Contact_With_An_Update.py
@@ -1,1 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Who To Contact With An Update", layout="wide")
+st.title("Who To Contact With An Update")
+
+st.image(
+    "assets/comms/Communication_Contacts.PNG",
+    caption="Communication Contacts",
+    use_column_width=True,
+)
+
+st.image(
+    "assets/comms/Communication_Scenarios.PNG",
+    caption="Communication Scenarios",
+    use_column_width=True,
+)
 

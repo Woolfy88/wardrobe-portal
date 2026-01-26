f41c0c009af7b9d92d90d1d582ec88b3402a5966
Woolfy88 <jwoolfenden@hdhc.com>
2026-01-26 09:57:51 +0000

    Update Field_Aware_Videos.py

--- pages/Field_Aware_Videos.py
+++ pages/Field_Aware_Videos.py
@@ -1,5 +1,4 @@
-wardrobe-portal/pages/pages
-/
-Field_Aware_Videos.py
-in
-main
+import streamlit as st
+
+st.title("Field Aware Videos")
+st.write("If you can see this, the page is wired up correctly.")
